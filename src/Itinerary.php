8b03334b66e9104fa4c8c99af623c899da32ff02
Nikita Ionov <ionov.goo@gmail.com>
2021-04-08 16:20:23 +0300

    fixed itinerary

--- src/Itinerary.php
+++ src/Itinerary.php
@@ -2,8 +2,51 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+function flatten($arr)
+{
+    $result = array_reduce($arr, function ($acc, $item) {
+        if (is_array($item)) {
+            return array_merge($acc, flatten($item));
+        }
+        $acc[] = $item;
+        return $acc;
+    }, []);
+    return $result;
+}
+
+function iter($tree, $value, $acc = [])
+{
+    [$name, $children] = $tree;
+    
+    if ($name == $value) {
+            $acc[] = $name;
+            return $acc;
+    }
+
+    if ($children == null) {
+        if ($name == $value) {
+            $acc[] = $name;
+            return $acc;
+        } else {
+            return;
+        }
+    }
+
+    $acc[] = $name;
+    $arr = flatten(array_map(fn($child)=>iter($child, $value, $acc), $children));
+    $filtered = array_values(array_filter($arr, fn($item)=>($item != null)));
+    return $filtered;
+}
+
 function itinerary($tree, $value1, $value2)
 {
+    $route1 = array_reverse(iter($tree, $value1));
+    print_r($route1);
+    $route2 = iter($tree, $value2);
+    print_r($route2);
+    $result = array_values(array_unique(array_merge($route1, $route2)));
+    print_r($result);
+    print_r(array_merge($route1, $route2));
     return $result;
 }
 
@@ -32,6 +75,11 @@ function test()
     ]];
 
     $colors = new App\Colors();
+
+    // print_r(iter($tree, 'Ivanovo'));
+    // print_r(iter($tree, 'Kursk'));
+    // print_r(iter($tree, 'Voronezh'));
+
 
     itinerary($tree, 'Dubna', 'Kostroma') === ['Dubna', 'Tver', 'Moscow', 'Ivanovo', 'Kostroma']
         ? print $colors->getColoredString(" OK ", "while", "green") . "\n"
